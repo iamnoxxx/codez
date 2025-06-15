edde240f754c88a1217bf9fbe49333deb2e93bee
iamnoxxx <nguyenphong0976332087@gmail.com>
2025-06-15 11:11:10 +0700

    Add files via upload

--- Vxezehubbountydemo.lua
+++ Vxezehubbountydemo.lua
@@ -1,79 +1,32 @@
 getgenv().Config = {
     ["Team"] = "Pirates",
-    ["Safe Health"] = {40, 50}, --- 60 - 70
-    ["Skip"] = {
-        ["Fruit"] = {
-            ["Enabled"] = true, --- false
-            ["Avoid Fruit"] = {
-                "Portal-Portal",
-                "Kitsune-Kitsune"
-            }
+    ["Config"] = {
+        ["Auto V4"] = true,
+        ["Skip Race V4 Players"] = true,
+        ["Low Health"] = 4500, 5300
+    },
+    ["Tool"] = {
+        ["Melee"] = {["Enable"] = true,
+            ["Z"] = {true, 0.7},
+            ["X"] = {true, 0.0},
+            ["C"] = {true, 0.0}
         },
-        ["Avoid V4"] = true --- false
-    },
-    ["Hunt Method"] = {
-        ["Use Move Predict"] = true, --- false
-        ["Hit and Run"] = true --- false
-    },
-    ["Spam All Skill On V4"] = {
-        ["Enabled"] = true, --- false
-        ["Weapons"] = {"Melee", "Gun", "Sword", "Blox Fruit"}
-    },
-    ["Chat"] = {
-        ["Enabled"] = false, --- false
-        ["Message"] = {"Vxeze Hub Auto Bounty"}
-    },
-    ["Custom Y Run"] = {
-        ["Enabled"] = true, --- false
-        ["Y Run"] = 5000 --- <5000
-    },
-    ["Misc"] = {
-        ["Auto Teleport Sea 2"] = false,
-        ["Auto Teleport Sea 3"] = true,
-        ["Auto Store Fruit"] = true,
-        ["Auto Random Fruit"] = true,
-        ["White Screen"] = false, --- true
-        ["Disabled Notify"] = false,
-        ["Boots FPS"] = false,
-        ["Auto Dash"] = true,
-        ["Auto Rejoin"] = true,
-        ["Click Delay"] = 0.01
-    },
-    ["Items"] = {
-        ["Use"] = {"Melee", "Gun", "Sword"},
-        ["Melee"] = {
-            ["Enable"] = true,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 1.5},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.1},
-                ["C"] = {["Enable"] = true, ["HoldTime"] = 0.1}
-            }
+        ["Blox Fruit"] = {["Enable"] = false,
+            ["Z"] = {true, 0},
+            ["X"] = {false, 0},
+            ["C"] = {false, 0},
+            ["V"] = {false, 0},
+            ["F"] = {true, 0.3}
         },
-        ["Blox Fruit"] = {
-            ["Enable"] = true,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.1},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.1},
-                ["C"] = {["Enable"] = true, ["HoldTime"] = 0.1},
-                ["V"] = {["Enable"] = false, ["HoldTime"] = 0.1},
-                ["F"] = {["Enable"] = true, ["HoldTime"] = 0.1}
-            }
+        ["Sword"] = {["Enable"] = true,
+            ["Z"] = {true, 1.5},
+            ["X"] = {true, 0.0}
         },
-        ["Sword"] = {
-            ["Enable"] = true,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.1},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.1}
-            }
+        ["Gun"] = {["Enable"] = false,
+            ["Z"] = {true, 0.1},
+            ["X"] = {true, 0.0}
         },
-        ["Gun"] = {
-            ["Enable"] = true,
-            ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0.1},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.1}
-            }
-        }
-    }
+    },
 }
 
 repeat task.wait() until game:IsLoaded()
